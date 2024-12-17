e0e233c7d6ce99d0103faf64ec9bc89dc33e50cb
RainRemm <rain.remmelgas@tktk.ee>
2024-12-17 03:36:30 +0200

    add week5 code

---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,12 @@
+﻿//AND - &&
+
+// true && true && true--> true
+// true && false --> false
+//flase && true --> false
+//false && false --> false       
+
+//OR - || (pipes)
+//true || true --> true
+//true || false --> true
+// flase || true --> true
+// false || false --> false
